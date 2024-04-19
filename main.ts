66f66b2d00a8a8c690bfe9272bb5b38dd8a48a34
WhyK <windchimeyk@gmail.com>
2024-04-19 19:50:29 +0900

    replace esm.sh to npm

--- main.ts
+++ main.ts
@@ -1,16 +1,8 @@
-import { Buffer } from "node:buffer";
-import process from "node:process";
 import { Hono, HTTPException } from "hono/mod.ts";
 import { contentType } from "std/media_types/mod.ts";
 import { Status } from "std/http/http_status.ts";
 import { serve } from "std/http/mod.ts";
-// @deno-types=https://esm.sh/v126/qrcode@1.5.3
-import * as Qrcode from "qrcode";
-
-// @ts-ignore: グローバル変数にブチ込む必要があるため
-globalThis.Buffer = Buffer;
-// @ts-ignore: グローバル変数にブチ込む必要があるため
-globalThis.process = process;
+import Qrcode from "qrcode";
 
 const app = new Hono();
 
